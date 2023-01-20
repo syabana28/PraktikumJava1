42f1ec788296d98f743ef1d924c67444b951170e
syabana28 <119665955+syabana28@users.noreply.github.com>
2023-01-20 09:20:24 +0700

    java

---
+++ Modul01/DataTypes.java
@@ -0,0 +1,13 @@
+public class DataTypes{
+ public static void main(String[] args){
+
+   String namaDepan ="Ahmad";
+   String namaTengah ="Sya";
+   String namaBelakang ="Bana";
+   int usia = 18:
+   int targettahunKuliah = 4;
+   double ipk = 3.897654321;
+   char nilaiAbjad = 'A';
+   boolean tampan = true;
+ }
+}
